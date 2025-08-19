795787230f93a43e243b4001cc7b92523e11cdd9
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-19 08:55:46 -0300

    feat: rota de login e page de login

---
+++ src/app/pages/login-page/login-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginLayoutDefault } from "../../components/login-layout-default/login-layout-default";
+
+@Component({
+  selector: 'app-login-page',
+  imports: [LoginLayoutDefault],
+  templateUrl: './login-page.html',
+  styleUrl: './login-page.css'
+})
+export class LoginPage {
+
+}
