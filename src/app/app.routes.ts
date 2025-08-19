0317a21ee274e15d5c1e1a4f12b8c32859a6e94a
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-19 11:22:59 -0300

    feat: path de medico, paciente e cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { MedicoPage } from './pages/medico-page/medico-page';
+import { PacientePage } from './pages/paciente-page/paciente-page';
+import { CadastroPage } from './pages/cadastro-page/cadastro-page';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPage },
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'medico', component: MedicoPage},
+    { path: 'paciente', component: PacientePage},
+    { path: 'cadastro', component: CadastroPage}
 ];
