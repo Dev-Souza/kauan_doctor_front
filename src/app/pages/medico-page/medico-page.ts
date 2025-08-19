ba85a013eb4a2a434400c5e899252353490ad631
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-19 11:24:32 -0300

    init: component page de medico

---
+++ src/app/pages/medico-page/medico-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medico-page',
+  imports: [],
+  templateUrl: './medico-page.html',
+  styleUrl: './medico-page.css'
+})
+export class MedicoPage {
+
+}
