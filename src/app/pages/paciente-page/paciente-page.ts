2bd3609a0cd063266dcf8701e60750d34cab2f28
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 07:41:37 -0300

    feat: import do painel no PacientePage

--- src/app/pages/paciente-page/paciente-page.ts
+++ src/app/pages/paciente-page/paciente-page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderLogado } from "../../components/headers/header-logado/header-logado";
+import { PainelMedicoComponent } from "../../components/painel-medico/painel-medico";
+import { PainelPaciente } from "../../components/painel-paciente/painel-paciente";
 
 @Component({
   selector: 'app-paciente-page',
-  imports: [HeaderLogado],
+  imports: [HeaderLogado, PainelPaciente],
   templateUrl: './paciente-page.html',
   styleUrl: './paciente-page.css'
 })
