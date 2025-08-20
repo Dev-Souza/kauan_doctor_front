412dcd7f91db9f60e82de9ac429bb0d4d1ddfd5e
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 13:02:03 -0300

    fix: add headers para fazer a req

--- src/app/services/consulta/cadastro-consulta.ts
+++ src/app/services/consulta/cadastro-consulta.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { CadastroConsultaPayload } from '../../models/cadastro-consulta-payload';
 import { Observable } from 'rxjs';
 
@@ -12,8 +12,16 @@ export class CadastroConsulta {
 
   constructor(private http: HttpClient) { }
 
+
   // Fazendo a requisição
-  cadastrarConsulta(payload: CadastroConsultaPayload) : Observable<any> {
-    return this.http.post(this.apiUrl, payload);
+  cadastrarConsulta(payload: CadastroConsultaPayload): Observable<any> {
+    // token
+    const token = localStorage.getItem('token');
+
+    // headers
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    })
+    return this.http.post(this.apiUrl, payload, {headers});
   }
 }
