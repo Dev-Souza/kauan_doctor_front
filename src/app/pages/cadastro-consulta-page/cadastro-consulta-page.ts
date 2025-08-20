1d8711ec85e01f13908d1e62a3f962bfa0ecb4a7
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 13:02:50 -0300

    feat: component de cadastrar uma consulta

--- src/app/pages/cadastro-consulta-page/cadastro-consulta-page.ts
+++ src/app/pages/cadastro-consulta-page/cadastro-consulta-page.ts
@@ -1,39 +1,95 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CadastroConsultaPayload } from '../../models/cadastro-consulta-payload';
 import { StatusConsulta } from '../../models/enums/status-consulta-enum';
 import { HeaderPattern } from "../../components/headers/header-pattern/header-pattern";
+import { CadastroConsulta } from '../../services/consulta/cadastro-consulta';
+import { Router } from '@angular/router';
+import { ListarMedicos } from '../../services/medico/listar-medicos';
+import { HorariosDisponiveis } from '../../services/agenda/horarios-disponiveis';
 
 @Component({
   selector: 'app-cadastro-consulta-page',
-  imports: [CommonModule, FormsModule, HeaderPattern],
+  imports: [CommonModule, FormsModule, HeaderPattern, ReactiveFormsModule],
   templateUrl: './cadastro-consulta-page.html',
   styleUrl: './cadastro-consulta-page.css'
 })
 export class CadastroConsultaPage {
-  pacienteId = 123; // ID do paciente logado
+  cadastroForm!: FormGroup;
+  errorMessage: string = '';
 
-  consulta: CadastroConsultaPayload = {
-    medico_id: 0,
-    paciente_id: this.pacienteId,
-    agenda_id: 0,
-    observacao: '',
-    status: StatusConsulta.Marcada
-  };
+  // medicos que irão aparecer no form
+  medicos: any[] = [];
 
-  medicos = [
-    { id: 1, nome: 'Dr. João Silva' },
-    { id: 2, nome: 'Dra. Maria Oliveira' }
-  ];
+  // horarios disponiveis de um médico
+  horariosDisponiveis: any[] = [];
 
-  horariosDisponiveis = [
-    { id: 101, dia: '2025-08-21', hora: '14:00' },
-    { id: 102, dia: '2025-08-22', hora: '10:30' }
-  ];
+  constructor(private fb: FormBuilder, private cadastraConsultaService: CadastroConsulta, private medicoService: ListarMedicos, private agendaService: HorariosDisponiveis, private router: Router) {
+    this.cadastroForm = this.fb.group({
+      medico_id: ['#', [Validators.required]],
+      paciente_id: [this.pacienteId, [Validators.required]],
+      agenda_id: [{value: '#', disabled: true}, [Validators.required]],
+      observacao: ['', [Validators.required]],
+      status: [StatusConsulta.Marcada, [Validators.required]]
+    })
+  }
 
-  enviarConsulta() {
+  // Pegando Id da pessoa que logou
+  pacienteId = localStorage.getItem('idLogado')
+
+  // consultar medicos no backend
+  ngOnInit(): void {
+    this.medicoService.listarMedicos().subscribe({
+      next: (dados) => {
+        console.log("Medicos: " + JSON.stringify(dados))
+        this.medicos = dados
+      },
+      error: (err) => console.error('Erro ao buscar médicos: ', err)
+    }); // Função para buscar médicos
+
+    this.cadastroForm.get('medico_id')?.valueChanges.subscribe(medicoId => {
+      const agendaControl = this.cadastroForm.get('agenda_id');
+
+      if(medicoId && medicoId !== '#') {
+        agendaControl?.enable();
+        this.agendaService.horariosDisponiveis(medicoId).subscribe({
+          next: (dados) => {
+            console.log(dados);
+            this.horariosDisponiveis = dados
+          },
+          error: (err) => console.error('Erro ao buscar Horários: ', err)
+        }); // Função para buscar horarios disponiveis
+      } else {
+        agendaControl?.disable();
+        agendaControl?.reset({ value: '#', disabled: true});
+        this.horariosDisponiveis = []
+      }
+    })
+  }
+
+  cadastrarConsulta() {
     // aqui você chama o service para enviar o payload
-    console.log('Consulta enviada:', this.consulta);
+    if(this.cadastroForm.valid){
+      const payload: CadastroConsultaPayload = {
+        medico_id: this.cadastroForm.value.medico_id,
+        paciente_id: Number(this.pacienteId),
+        agenda_id: this.cadastroForm.value.agenda_id,
+        observacao: this.cadastroForm.value.observacao,
+        status: StatusConsulta.Marcada
+      }
+
+      this.cadastraConsultaService.cadastrarConsulta(payload).subscribe({
+        next: (res) => {
+          console.log(res)
+          alert('Consulta cadastrada com sucesso!');
+          this.router.navigate(['/paciente'])
+        },
+        error: (err) => {
+          console.error('Erro no cadastro:', err);
+          this.errorMessage = "Erro ao cadastrar consulta";
+        }
+      })
+    }
   }
 }
