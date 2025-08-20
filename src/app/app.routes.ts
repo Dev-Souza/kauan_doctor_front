28ab0f67e504efc1718655a6233cb31b32a6b07e
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-20 07:46:02 -0300

    config: path de consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginPage } from './pages/login-page/login-page';
 import { MedicoPage } from './pages/medico-page/medico-page';
 import { PacientePage } from './pages/paciente-page/paciente-page';
 import { CadastroPage } from './pages/cadastro-page/cadastro-page';
+import { CadastroConsultaPage } from './pages/cadastro-consulta-page/cadastro-consulta-page';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPage },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'medico', component: MedicoPage},
-    { path: 'paciente', component: PacientePage},
-    { path: 'cadastro', component: CadastroPage}
+    { path: 'medico', component: MedicoPage },
+    { path: 'paciente', component: PacientePage },
+    { path: 'cadastro', component: CadastroPage },
+    { path: 'consulta', component: CadastroConsultaPage }
 ];
