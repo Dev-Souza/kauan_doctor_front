2ce1f0121d51a0da9f68d8c141e273ec2d8ca2bd
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-21 11:04:44 -0300

    feat: import header pattern na page de cadastro user

--- src/app/pages/cadastro-page/cadastro-page.ts
+++ src/app/pages/cadastro-page/cadastro-page.ts
@@ -4,10 +4,11 @@ import { CadastrarService } from '../../services/cadastro/cadastrar-service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CadastroPayload } from '../../models/cadastro-payload';
+import { HeaderPattern } from "../../components/headers/header-pattern/header-pattern";
 
 @Component({
   selector: 'app-cadastro-page',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HeaderPattern],
   templateUrl: './cadastro-page.html',
   styleUrl: './cadastro-page.css'
 })
