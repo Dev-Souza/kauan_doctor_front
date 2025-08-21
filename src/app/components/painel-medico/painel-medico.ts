5514cb7fbf52c977a9210901c54132e31267f8ed
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-21 10:41:34 -0300

    fix: agrupanda horarios pela data e trazendo dados de consulta

--- src/app/components/painel-medico/painel-medico.ts
+++ src/app/components/painel-medico/painel-medico.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HorariosDisponiveis } from '../../services/agenda/horarios-disponiveis';
+import { BuscarConsultasDeUmMedico } from '../../services/consulta/buscar-consultas-de-um-medico';
+import { BuscarPacientePeloId } from '../../services/paciente/buscar-paciente-pelo-id';
+import { BuscarAgendaPeloId } from '../../services/agenda/buscar-agenda-pelo-id';
 
 @Component({
   selector: 'app-painel-medico',
@@ -8,24 +12,83 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './painel-medico.html',
   styleUrl: './painel-medico.css'
 })
-
 export class PainelMedicoComponent {
-  agendaDisponivel = [
-    { dia: 'Segunda-feira', horas: ['08:00', '09:00', '10:00'] },
-    { dia: 'Terça-feira', horas: ['13:00', '14:00'] },
-    { dia: 'Quarta-feira', horas: ['08:00', '11:00', '15:00'] },
-  ];
+  agendaDisponivel: any[] = [];
+  agendaAgrupada: { dataAgenda: string, horarios: string[] }[] = [];
 
-  consultas = [
-    { paciente: 'João Silva', data: '20/08/2025', hora: '09:00' },
-    { paciente: 'Maria Oliveira', data: '21/08/2025', hora: '14:00' },
-  ];
+  consultas: any[] = [];
 
-  anotacoes: string = '';
+  // Consultas já com todos os dados
+  consultasCompletas: any[] = [];
 
-  salvarAnotacoes() {
-    alert('Anotações salvas com sucesso!');
-    console.log('Anotações:', this.anotacoes);
+  // Pegando id do médico
+  idMedicoLogado = localStorage.getItem('idLogado');
+
+  constructor(
+    private agendaService: HorariosDisponiveis,
+    private agendaServiceGetById: BuscarAgendaPeloId,
+    private consultaService: BuscarConsultasDeUmMedico,
+    private pacienteService: BuscarPacientePeloId
+  ) { }
+
+  ngOnInit(): void {
+    this.agendaService.horariosDisponiveis(Number(this.idMedicoLogado)).subscribe({
+      next: (dados) => {
+        this.agendaDisponivel = dados;
+        this.agendaAgrupada = this.agruparPorData(dados);
+      },
+      error: (err) => console.error('Erro ao buscar Horários: ', err)
+    });
+
+    this.consultaService.getConsultasMedicoById(Number(this.idMedicoLogado)).subscribe({
+      next: (dados) => {
+        this.consultas = dados;
+        this.carregarDadosCompletos(this.consultas);
+      },
+      error: (err) => console.error('Erro ao buscar consultas: ', err)
+    });
   }
-}
 
+  agruparPorData(agenda: any[]): { dataAgenda: string, horarios: string[] }[] {
+    const mapa = new Map<string, string[]>();
+
+    for (const item of agenda) {
+      const data = item.dataAgenda;
+      const hora = item.horaInicio;
+
+      if (!mapa.has(data)) {
+        mapa.set(data, []);
+      }
+
+      mapa.get(data)!.push(hora);
+    }
+
+    return Array.from(mapa.entries()).map(([dataAgenda, horarios]) => ({
+      dataAgenda,
+      horarios
+    }));
+  }
+
+
+  carregarDadosCompletos(consultas: any[]) {
+    this.consultasCompletas = [];
+
+    consultas.forEach(consulta => {
+      const paciente$ = this.pacienteService.getPacienteById(consulta.paciente_id);
+      const agenda$ = this.agendaServiceGetById.getAgendaById(consulta.agenda_id);
+
+      // Combina as duas chamadas
+      paciente$.subscribe(paciente => {
+        agenda$.subscribe(agenda => {
+          this.consultasCompletas.push({
+            paciente,
+            agenda,
+            observacoes: consulta.observacoes,
+            status: consulta.status
+          });
+        });
+      });
+    });
+  }
+
+}
